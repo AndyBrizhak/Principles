fd5bb46ab95fe69a2374eed79d430a2e540e43ee
AndyBrizhak <andrewbrizhak@gmail.com>
2019-04-28 15:07:13 +0800

    +DRY

---
+++ Dry/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Dry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DummyFunc();
+            DummyFuncAgain();
+            DummyFuncMore();
+            Console.ReadLine();
+        }
+
+        private static void DummyFunc()
+        {
+            string address = "Москва, Россия";
+            string format = "{0} - {1}, адрес {2}, возраст {3}";
+            Console.WriteLine(format, "Петя", "школьный друг", address, 30);
+        }
+
+        private static void DummyFuncAgain()
+        {
+            string address = "Москва, Россия";
+            string format = "{0} - {1}, адрес {2}, возраст {3}";
+            Console.WriteLine(format, "Вася", "сосед", address, 54);
+        }
+        private static void DummyFuncMore()
+        {
+            string address = "Москва, Россия";
+            string format = "{0} - {1}, адрес {2}, возраст {3}";
+            Console.WriteLine(format, "Николай", "сын", address, 4);
+        }
+    }
+}
