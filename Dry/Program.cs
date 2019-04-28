598e0bab5ab13784db2129a4a848f2793f23a46f
AndyBrizhak <andrewbrizhak@gmail.com>
2019-04-28 15:38:28 +0800

    +метод WriteToConsole

--- Dry/Program.cs
+++ Dry/Program.cs
@@ -5,6 +5,8 @@ namespace Dry
 {
     class Program
     {
+        public static readonly string Address = Constants.Address;
+        public static readonly string Format = Constants.Format;
         static void Main(string[] args)
         {
             DummyFunc();
@@ -13,24 +15,32 @@ namespace Dry
             Console.ReadLine();
         }
 
+        private static void WriteToConsole(string name, string description, int age)
+        {
+            Console.WriteLine(Format, name, description, Address, age);
+        }
+
         private static void DummyFunc()
         {
-            string address = Constants.Address;
-            string format = Constants.Format;
-            Console.WriteLine(format, "Петя", "школьный друг", address, 30);
+            //string address = Constants.Address;
+            //string format = Constants.Format;
+            //Console.WriteLine(Format, "Петя", "школьный друг", Address, 30);
+            WriteToConsole("Петя", "школьный друг", 30);
         }
 
         private static void DummyFuncAgain()
         {
-            string address = Constants.Address;
-            string format = Constants.Format;
-            Console.WriteLine(format, "Вася", "сосед", address, 54);
+            //string address = Constants.Address;
+            //string format = Constants.Format;
+            //Console.WriteLine(Format, "Вася", "сосед", Address, 54);
+            WriteToConsole("Вася", "сосед", 54);
         }
         private static void DummyFuncMore()
         {
-            string address = Constants.Address;
-            string format = Constants.Format;
-            Console.WriteLine(format, "Николай", "сын", address, 4);
+            //string address = Constants.Address;
+            //string format = Constants.Format;
+            //Console.WriteLine(Format, "Николай", "сын", Address, 4);
+            WriteToConsole("Николай", "сын", 4);
         }
     }
 }
