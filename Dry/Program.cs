031daae3747dad6ac71b7237cc9bb545759e6876
AndyBrizhak <andrewbrizhak@gmail.com>
2019-04-28 15:20:57 +0800

    +constants class

--- Dry/Program.cs
+++ Dry/Program.cs
@@ -15,21 +15,21 @@ namespace Dry
 
         private static void DummyFunc()
         {
-            string address = "Москва, Россия";
-            string format = "{0} - {1}, адрес {2}, возраст {3}";
+            string address = Constants.Address;
+            string format = Constants.Format;
             Console.WriteLine(format, "Петя", "школьный друг", address, 30);
         }
 
         private static void DummyFuncAgain()
         {
-            string address = "Москва, Россия";
-            string format = "{0} - {1}, адрес {2}, возраст {3}";
+            string address = Constants.Address;
+            string format = Constants.Format;
             Console.WriteLine(format, "Вася", "сосед", address, 54);
         }
         private static void DummyFuncMore()
         {
-            string address = "Москва, Россия";
-            string format = "{0} - {1}, адрес {2}, возраст {3}";
+            string address = Constants.Address;
+            string format = Constants.Format;
             Console.WriteLine(format, "Николай", "сын", address, 4);
         }
     }
