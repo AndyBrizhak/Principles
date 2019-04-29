09f75bcf386bd1ab7dc9dd84ffeee55397e1bb16
AndyBrizhak <andrewbrizhak@gmail.com>
2019-04-29 09:12:05 +0800

    +magic numbers example

---
+++ MagicNumbers/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicNumbers
+{
+    /// <summary>
+    /// Describe Animal
+    /// </summary>
+    class Animal
+    {
+        public string AnimalName { get; set; }
+        public int LegsCount { get; set; }
+        public string AnimalType { get; set; }
+    }
+}
---
+++ MagicNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MagicNumbers
+{
+    
+    class Program
+    {
+        /// <summary>
+        /// Give Collection Animals
+        /// </summary>
+        /// <returns></returns>
+        private static IEnumerable<Animal> GetAnimals()
+        {
+            return new List<Animal>()
+            {
+                new Animal(){
+                    LegsCount = 4 ,
+                    AnimalType = "Млекопитающие" ,
+                    AnimalName = "Собака"
+                }
+                , new Animal(){
+                    LegsCount = 4 ,
+                    AnimalType = "Млекопитающие" ,
+                    AnimalName = "Корова"
+                }
+                , new Animal(){
+                    LegsCount = 2 ,
+                    AnimalType = "Птицы" ,
+                    AnimalName = "Утка"
+                }
+            };
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
