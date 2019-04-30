73d015f0b8bab227543574efdf1ba6ccaba8706e
AndyBrizhak <andrewbrizhak@gmail.com>
2019-05-01 07:29:37 +0800

    +Repeat logic

---
+++ RepeatLogic/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace RepeatLogic
+{
+    class Customer
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+
+        #region Customer()
+
+        public Customer()
+        {
+            Id = CalculateId();
+        }
+
+        #endregion
+
+        #region CalculateId()
+
+        private long CalculateId()
+        {
+            long id = DateTime.Now.Ticks;
+            return id;
+        }
+
+        #endregion
+    }
+}
