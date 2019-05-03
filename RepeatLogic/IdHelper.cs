a7650e21f5dde5f74b59c6ac414c1a82fcbd1c28
AndyBrizhak <andrewbrizhak@gmail.com>
2019-05-03 20:58:56 +0800

    +IdHelper

---
+++ RepeatLogic/IdHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+#region ? Проблема
+//Использование статических методов часто сигнализирует о сильной связанности.
+//В данном случае сильные связи протянулись от классов Customer и Store к классу IdHelper
+#endregion
+
+public static class IdHelper
+{
+    public static long CalculateId()
+    {
+        long id = DateTime.Now.Ticks;
+        return id;
+    }
+}
