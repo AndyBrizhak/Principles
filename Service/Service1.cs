035a39e5a1eebeac86575cf55e5e0c9ba419a83a
AndyBrizhak <andrewbrizhak@gmail.com>
2019-05-06 19:24:46 +0800

    +

--- Service/Service1.cs
+++ Service/Service1.cs
@@ -6,5 +6,9 @@ namespace Service
    public class Service1
    {
        public string Data { get; set; }
+       public string LoadFromFile(string Path) => string.Empty;
+       public string LoadFromUrl(string Url) => string.Empty;
+
    }
+
 }
