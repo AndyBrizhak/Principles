f7751c543ca21a2b1e877c4e856f208b5db2c115
AndyBrizhak <andrewbrizhak@gmail.com>
2019-05-13 08:47:00 +0800

    +class LoadService

---
+++ Service/LoadService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class LoadService
+    {
+        public string LoadFromFile(string Path) => throw new NotImplementedException();
+        public string LoadFromUrl(string Url) => throw new NotImplementedException();
+
+    }
+}
---
+++ Service/Service2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class Service2
+    {
+        public string Data { get; set; }
+        public string LoadFromFile(string Path) => string.Empty;
+        public string LoadFromUrl(string Url) => string.Empty;
+        public void SendEmailData(string Email, string Subject, string Text) { }
+        public void SendFtpData(string FtpAddres, string Port, Account User) { }
+        public void SaveXmlData(List<string> DataList) { }
+        public void SaveJsonData(List<string> DataList) { }
+    }
+}
