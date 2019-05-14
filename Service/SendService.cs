615808aed598e2e15afabdbbe4e41d6a7d14ddd0
AndyBrizhak <andrewbrizhak@gmail.com>
2019-05-14 19:12:43 +0800

    + class SendService

---
+++ Service/SendService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class SendService
+    {
+        public int Date { get; set; }
+        public void SendEmailDate(string Email, string Subject, string Text) { }
+        public void SendFtpDate(string FtpAddres, string Port, Account User) { }
+
+    }
+}
